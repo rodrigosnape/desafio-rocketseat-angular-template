2468a03d9dde1d373b9f797ad9ffacdbec2ecd5a
Rodrigo Veiga <rodrigo.veiga@grupoccaa.com.br>
2025-09-30 20:39:48 -0300

    Aula 2 - Criação da lógica de login

--- frontend-gestao-marketplace/src/app/pages/login/login.ts
+++ frontend-gestao-marketplace/src/app/pages/login/login.ts
@@ -1,11 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
+import { UserService } from '../../services/user.service';
+import { UserAuthService } from '../../services/user-auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
 export class Login {
 
+  loginErrorMessage = '';
+
+  userForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required])
+  });
+
+  private readonly _userService = inject(UserService);
+  private readonly _userAuthService = inject(UserAuthService);
+  private readonly _router = inject(Router);
+
+  login() {
+    if(this.userForm.invalid) return;
+
+    this._userService.login(this.userForm.get('email')?.value as string, this.userForm.get('password')?.value as string).subscribe({
+      next: (response) => {
+        this.loginErrorMessage = '';
+
+        // salvar o token no localstorage
+        this._userAuthService.setUserToken(response.data.token);
+
+        // redirecionar para tela de produtos
+        this._router.navigate(['/products']);
+      },
+      error: (error) => {
+        console.log('Erro ao fazer login', error);
+        this.loginErrorMessage = error.error.message;
+      },
+    });
+
+  }
 }
--- frontend-gestao-marketplace/src/app/services/user-auth.service.ts
+++ frontend-gestao-marketplace/src/app/services/user-auth.service.ts
@@ -5,7 +5,12 @@ import { Injectable } from '@angular/core';
 })
 export class UserAuthService {
   getUserToken() {
-    // TODO: Recuperar token do localstorage;
-    return 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImVtYWlsIjoibm92b0BleGFtcGxlLmNvbSIsImlhdCI6MTc1OTI2NjgyMiwiZXhwIjoxNzU5MzUzMjIyfQ.3XznRcsDqjClYhu7pZPvbVfhsHvoaWqJvyeUhz7HPUk';
+
+    //return 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImVtYWlsIjoibm92b0BleGFtcGxlLmNvbSIsImlhdCI6MTc1OTI2NjgyMiwiZXhwIjoxNzU5MzUzMjIyfQ.3XznRcsDqjClYhu7pZPvbVfhsHvoaWqJvyeUhz7HPUk';
+    return localStorage.getItem('auth-token') || '';
+  }
+
+  setUserToken(token: string) {
+    localStorage.setItem('auth-token', token);
   }
 }
