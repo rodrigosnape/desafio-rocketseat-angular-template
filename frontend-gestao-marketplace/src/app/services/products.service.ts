57cbd4a918605c031968f408e57e671d8fcbc79f
Rodrigo Veiga <rodrigo.veiga@grupoccaa.com.br>
2025-10-01 15:17:02 -0300

    Aula 3 - Criação da lógica de listagem e filtro de produtos

--- frontend-gestao-marketplace/src/app/services/products.service.ts
+++ frontend-gestao-marketplace/src/app/services/products.service.ts
@@ -3,6 +3,7 @@ import { inject, Injectable } from '@angular/core';
 import { INewProductRequest } from '../interfaces/new-product-request';
 import { INewProductResponse } from '../interfaces/new-product-response';
 import { Observable } from 'rxjs';
+import { IProductsResponse } from '../interfaces/products-response';
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +13,9 @@ export class ProductsService {
 
   saveProduct(product: INewProductRequest): Observable<INewProductResponse> {
     return this._httpClient.post<INewProductResponse>('http://localhost:3000/api/products', product);
+  }
+
+  getProducts(): Observable<IProductsResponse>{
+    return this._httpClient.get<IProductsResponse>('http://localhost:3000/api/products');
   }
 }
